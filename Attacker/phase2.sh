73d2ab0521d717aadd055afd34b4308d118745d4
d3f4ult-wind <lenamphong23062004@gmail.com>
2026-05-04 15:49:19 +0700

    Add Latency (Attacker) 2.0

--- Attacker/phase2.sh
+++ Attacker/phase2.sh
@@ -1,3 +1,3 @@
 # Gửi 1 request mỗi 1 giây
-ip netns exec ns10 bash -c 'while true; do curl -s -o /dev/null -w "%{time_total}\n" http://10.10.2.2; sleep 1; done' | tee -a latency.log
+ip netns exec ns10 bash -c 'while true; do curl -s -o /dev/null -w "%{time_total}\n" http://10.10.2.2  | tee -a latency.log; sleep 1; done'
 
